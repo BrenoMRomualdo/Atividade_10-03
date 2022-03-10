d69f3b6787dcb2cfb2b783586b4be81a68bb7347
BrenoMR <aluno@IPIRANGA.FATEC>
2022-03-10 17:40:55 -0300

    Atividade 2 SemiFinalizada

--- Exercicio_01.java
+++ Exercicio_01.java
@@ -1,3 +1,20 @@
-public class Exercicio_01 {
-    
+import java.util.*;
+public class Exercicio_01{
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+        System.out.printf ("Digite um número e eu farei uma magica!");
+        int numero = leitor.nextInt();
+        if (numero >0){
+            System.out.printf ("Usando meus poderes eu concluo que seu número é positivo!");
+        }
+        else{ 
+            if(numero <0){
+            System.out.printf ("Utilizando minha magia mágica eu digo que seu número é negativo");
+            }
+                else{
+                    if(numero =0)
+                    System.out.printf ("Usando todas as minhas forças de mago eu AFIRMO que seu número é neutro");
+            }
+        }
+    }
 }
--- Exercicio_02.java
+++ Exercicio_02.java
@@ -1,3 +1,35 @@
+import javax.swing.JOptionPane;
+import java.util.*;
 public class Exercicio_02 {
-    
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+        System.out.printf ("Vamos fazer uma equação de segundo grau\n Qual o valor de A?");
+        double a = leitor.nextDouble();
+        System.out.printf ("Qual o valor de B?");
+        double b = leitor.nextDouble();
+        System.out.printf ("Qual o valor de c?");
+        double c = leitor.nextDouble();
+        double delta, raiz_1, raiz_2;
+        delta = (b*b) - (4*a*c);
+
+        if (delta >0){
+            raiz_1 = (-b + Math.sqrt(delta))/2*a;
+            raiz_2 = (-b - Math.sqrt(delta))/2*a;
+            String s = String.format(
+                "Seu delta é maior que zero, então você tem 2 raizes: \n raiz 1 = %.0f \n raiz 2 = %.0f.",
+                raiz_1, raiz_2
+                );
+            JOptionPane.showInputDialog(null, s);
+        }
+        else if (delta = 0){
+            raiz_1 = (-b + Math.sqrt(delta))/2*a;
+            String r = String.format("Seu delta é igual a zero, ou seja, só tem 1 raiz:\n raiz = %.0f.",
+            raiz_1
+            );
+            JOptionPane.showInputDialog(null, r);
+        }
+        else {
+            JOptionPane.showInputDialog(null, "Seu delta é menor que zero, então não existe raiz real!");
+        }
+    }
 }
