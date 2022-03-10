411f3930de07dbe0a936fbd43372d3eb19d8be96
BrenoMR <aluno@IPIRANGA.FATEC>
2022-03-10 18:14:39 -0300

    Exercicios 1 e 2 feitos e 3 parcialmente feito

--- Exercicio_03.java
+++ Exercicio_03.java
@@ -1,3 +1,19 @@
+import javax.swing.JOptionPane;
+import java.util.*;
 public class Exercicio_03 {
-    
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+        int n = 10;
+        int v[] = new int [n];
+        System.out.printf ("fale um número");
+        int numero1 = leitor.nextInt();
+        System.out.printf ("Fale outro número");
+        int numero2 = leitor.nextInt();
+        System.out.printf ("Fale o ultimo número");
+        int numero3 = leitor.nextInt();
+        if (numero1 >numero2 ){
+            
+        }
+
+    }
 }
